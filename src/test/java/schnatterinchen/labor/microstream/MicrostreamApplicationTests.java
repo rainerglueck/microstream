259902f06e7a1f455efe59d9d1233f628fcb29c6
Rainer Glück <rainer.glueck@vontobel.com>
2021-05-12 15:54:29 +0200

    initial commit

---
+++ src/test/java/schnatterinchen/labor/microstream/MicrostreamApplicationTests.java
@@ -0,0 +1,13 @@
+package schnatterinchen.labor.microstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
